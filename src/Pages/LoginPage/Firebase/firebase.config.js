a9e3495369c726de81cba9412d43a1046c209b84
Rabby-Cyber <ariyanrabby94@gmail.com>
2021-11-19 22:27:37 +0600

    local file added

--- src/Pages/LoginPage/Firebase/firebase.config.js
+++ src/Pages/LoginPage/Firebase/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBrs_rvIEvBQURvD7a-0i1qO6oqDFMFulU",
-  authDomain: "rent-nowio.firebaseapp.com",
-  projectId: "rent-nowio",
-  storageBucket: "rent-nowio.appspot.com",
-  messagingSenderId: "833394792058",
-  appId: "1:833394792058:web:e6d281fc6dd939464f0dc3",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 export default firebaseConfig;
